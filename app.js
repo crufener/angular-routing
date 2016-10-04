d67f411886084be8fca126b130d7142505759f9d
Craig Rufener <crufener@outlook.com>
2016-10-04 12:20:25 -0700

    initial setup
    using angular-ui-router for routing SPA'x

---
+++ app.js
@@ -0,0 +1,26 @@
+var MyApp;
+(function (MyApp) {
+    angular.module("MyApp", ["ui.router"]).config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
+        $stateProvider
+            .state("Home", {
+            url: '/',
+            templateUrl: "ngApp/page1.html",
+            controller: MyApp.Controllers.Page1Controller,
+            controllerAs: 'controller'
+        })
+            .state("Home", {
+            url: '/',
+            templateUrl: "ngApp/page1.html",
+            controller: MyApp.Controllers.Page1Controller,
+            controllerAs: 'controller'
+        })
+            .state("Page2", {
+            url: '/page2',
+            templateUrl: "ngApp/page2.html",
+            controller: MyApp.Controllers.Page2Controller,
+            controllerAs: 'controller'
+        });
+        $urlRouterProvider.otherwise('/');
+        $locationProvider.html5Mode(true);
+    });
+})(MyApp || (MyApp = {}));
---
+++ app.ts
@@ -0,0 +1,35 @@
+namespace MyApp {
+    angular.module("MyApp",["ui.router"]).config(
+        ($stateProvider: ng.ui.IStateProvider,
+        $locationProvider: ng.ILocationProvider,
+        $urlRouterProvider: ng.ui.IUrlRouterProvider)=>{
+            $stateProvider
+                .state(
+                    "Home", {
+                        url: '/',
+                        templateUrl: "ngApp/page1.html",
+                        controller: MyApp.Controllers.Page1Controller,
+                        controllerAs: 'controller'
+                    }
+                )
+                .state(
+                    "Home", {
+                        url: '/',
+                        templateUrl: "ngApp/page1.html",
+                        controller: MyApp.Controllers.Page1Controller,
+                        controllerAs: 'controller'
+                    }
+                )
+                .state(
+                    "Page2", {
+                        url: '/page2',
+                        templateUrl: "ngApp/page2.html",
+                        controller: MyApp.Controllers.Page2Controller,
+                        controllerAs: 'controller'
+                    }
+                );
+                $urlRouterProvider.otherwise('/');
+                $locationProvider.html5Mode(true);
+        }
+    )
+}
